6e2bf954794dd35bc53323fb41f3301ff62476f4
pagiannis <pagiannis04@gmail.com>
2025-04-10 18:22:32 +0300

    Controllers logic added

--- routes/products.js
+++ routes/products.js
@@ -1,26 +1,11 @@
 const express = require('express');
+const { getAllProducts, getProductById } = require('../controllers/products');
 const router = express.Router();
-const Product = require('../models/Product');
 
 // Get all products
-router.get('/', async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
+router.get('/', getAllProducts);
 
 // Get a single product by ID
-router.get('/:id', async (req, res) => {
-    try {
-      const product = await Product.findById(req.params.id);
-      if (!product) return res.status(404).json({ message: 'Product not found' });
-      res.json(product);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-});
+router.get('/:id', getProductById);
 
 module.exports = router;

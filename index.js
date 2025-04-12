81a1e79e7212eab296e52ca04d3ae6f5dc6ed8df
pagiannis <pagiannis04@gmail.com>
2025-04-12 22:48:58 +0300

    Fix cookies error using cors

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const productRoutes = require('./routes/products');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173','http://localhost:3000'],
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+}));
+
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);

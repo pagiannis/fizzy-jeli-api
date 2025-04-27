1c9f12cb905eef7a6d5e715d4cad3e5c59359115
pagiannis <pagiannis04@gmail.com>
2025-04-27 15:08:16 +0300

    Apply ContactForm middleware in the route

--- routes/contactForm.js
+++ routes/contactForm.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { postContactForm } = require('../controllers/contactForm');
+const validateContactForm = require('../middleware/validateContactForm');
 const router = express.Router();
 
 // Post the contact form
-router.post('/', postContactForm);
+router.post('/', validateContactForm, postContactForm);
 
 module.exports = router;

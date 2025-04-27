00535a4441a90d98e422862e9c855e8207de63a8
pagiannis <pagiannis04@gmail.com>
2025-04-27 15:05:39 +0300

    Validation middleware for ContactForm

---
+++ middleware/validateContactForm.js
@@ -0,0 +1,13 @@
+const { contactFormSchema } = require('../schemas/contactForm.schema');
+
+const validateContactForm = (req, res, next) => {
+    const { error } = contactFormSchema.validate(req.body); 
+
+    if (error) {
+        return res.status(400).json({
+            error: error.details[0].message 
+        });
+    }
+
+    next();
+};
--- middleware/validateProduct.js
+++ middleware/validateProduct.js
@@ -2,11 +2,13 @@ const { productSchema } = require('../schemas/product.schema');
 
 const validateProduct = (req, res, next) => {
     const { error } = productSchema.validate(req.body);
+    
     if (error) {
         return res.status(400).json({ 
             error: error.details[0].message 
         });
     }
+    
     next(); // Proceed if validation passes
 };
 

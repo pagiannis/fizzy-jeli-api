914253b52c9c4832dc55c3899e974eb9cf5412fb
pagiannis <pagiannis04@gmail.com>
2025-05-04 16:18:51 +0300

    Create space in users controller

--- controllers/users.js
+++ controllers/users.js
@@ -12,8 +12,10 @@ const postUser = async (req, res) => {
     if (user) return res.status(400).send('User already registered.');
 
     user = new User(_.pick(req.body, ['username', 'email', 'password', 'favourites', 'bag']));
+
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
+    
     await user.save();
     
     res.send(_.pick(user, ['_id','username', 'email', 'favourites', 'bag']));

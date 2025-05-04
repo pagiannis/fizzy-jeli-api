6fb9ed0313de03589eb290a7479307468b56440c
pagiannis <pagiannis04@gmail.com>
2025-05-04 16:16:57 +0300

    Hashing passwords

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const User = require('../models/User');
 
@@ -11,6 +12,8 @@ const postUser = async (req, res) => {
     if (user) return res.status(400).send('User already registered.');
 
     user = new User(_.pick(req.body, ['username', 'email', 'password', 'favourites', 'bag']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     await user.save();
     
     res.send(_.pick(user, ['_id','username', 'email', 'favourites', 'bag']));

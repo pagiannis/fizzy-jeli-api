c322a53686b43d9b2649a7de01d00f1e5083235c
pagiannis <pagiannis04@gmail.com>
2025-05-22 19:13:17 +0300

    Use .env variables instead of hardcoding values

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:3000'],
+    origin: [ process.env.FRONTEND_URL, process.env.BACKEND_URL ],
     credentials: true,
     exposedHeaders: ['set-cookie', 'x-auth-token'], 
 }));

4e4ca33760c3b392148517d119863dd37810e875
pagiannis <pagiannis04@gmail.com>
2025-07-03 19:53:44 +0300

    Change forgot passsword routes

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { sendResetCode } = require('../controllers/forgotPassword');
 
-router.post('/', sendResetCode);
+router.post('/request-reset-code', sendResetCode);
 
 module.exports = router;

12b9a49c67dc61edca0055372002bbe9f2fda2e7
pagiannis <pagiannis04@gmail.com>
2025-07-06 17:17:58 +0300

    Reset password route

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -21,6 +21,30 @@ const sendResetCode = async (req, res) => {
     });
 };
 
+const resetPassword = async (req, res) => {
+    const { token, newPassword } = req.body;
+    if (!token || !newPassword) {
+        return res.status(400).json({ message: 'Token and new password are required.' });
+    }
+
+    const user = await User.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpires: { $gt: Date.now() }
+    });
+
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid or expired token.' });
+    }
+
+    user.password = newPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+
+    return res.status(200).json({ message: 'Password has been reset successfully.' });
+};
+
 module.exports = {
     sendResetCode,
+    resetPassword,
 }
--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { sendResetCode } = require('../controllers/forgotPassword');
+const { resetPassword } = require('../controllers/forgotPassword');
 
 router.post('/request-reset-code', sendResetCode);
+
+router.patch('/reset-password', resetPassword);
 
 module.exports = router;
